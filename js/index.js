6b9a201f322e62e7ed9b88d60ba7c2e4b9b9c197
LHaupt1s <LHaupt1s@capetigers.com>
2026-05-07 08:23:13 -0500

    working on display

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ var sumbit = document.getElementById("sumbit");
 
 var pokeimg = document.getElementById("pokeimg");
 var pokename = document.getElementById("pokeName");
-var thepokemon = document.getElementById("thepokemon");
+var pokedexNum = document.getElementById("pokedexNum");
 
 
 // *Function for taking data from api
@@ -21,7 +21,7 @@ async function getData() {
     var searchInput = searchBar.value.toLowerCase();
 
 
-    const url = `https://pokeapi.co/api/v2/${groupValue}/${searchInput}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${searchInput}`;
     try {
 
         const response = await fetch(url, {
@@ -37,20 +37,42 @@ async function getData() {
         const result = `${pokeData}`
         console.log(result);
         
-        // General Result
+
+        // * Insert data onto webpage
 
         if (result == pokeData) {
+
             pokeimg.src = pokeData.sprites.front_default;
             pokeimg.style.display = "block";
 
             pokename.innerHTML = pokeData.name;
+            //pokedexNum.innerHTML = pokeData.pokedex_numbers;
+            //console.log(pokeData.pokedex_numbers);
+
+
+
+            const speciesurl = `https://pokeapi.co/api/v2/pokemon-species/${searchInput}`;
+
+            try {
+                const response = await fetch(speciesurl, {
+                    method: "GET"
+            });
+            if (!response.ok) {
+            throw new Error(`response status: ${response.status}`);
+            }
+
+
+            }catch (error) {
+                console.error(error.message);
+            }
+
+
+            
         }
 
     }catch (error) {
         console.error(error.message);
     }
-
-    // * Insert Data onto page
 
     
 }
