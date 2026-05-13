0aad59673b3b3a9901c6338578b1f559161acac9
LHaupt08 <LHaupt1s@capetigers.com>
2026-05-13 14:20:01 -0500

    fix display

--- js/index.js
+++ js/index.js
@@ -22,6 +22,9 @@ var statbar = [
 
 var bstbar = document.getElementById("bstbar");
 
+var carouselInd = document.getElementById("carouselIndicator");
+var carouselArea = document.getElementById("carouselArea");
+
 
 // *Function for taking data from api
 async function getData() {
@@ -125,17 +128,17 @@ async function getData() {
             bstbar.style.width=widthValue + "%";
 
             if (bsttotal < 50){
-                    bstbar.classList.add("bg-danger");
-                } else if(bsttotal >= 200 & bsttotal <= 449) {
-                    bstbar.classList.add("bg-warning");
-                } else if(bsttotal >= 450 & bsttotal <= 649){
-                    bstbar.classList.add("bg-success");
-                } else if(bsttotal >= 650) {
-                    bstbar.classList.add("bg-info");
-                }
+                bstbar.classList.add("bg-danger");
+            } else if(bsttotal >= 200 & bsttotal <= 449) {
+                bstbar.classList.add("bg-warning");
+            } else if(bsttotal >= 450 & bsttotal <= 649){
+                bstbar.classList.add("bg-success");
+            } else if(bsttotal >= 650) {
+                bstbar.classList.add("bg-info");
+            }
 
 
-            var pokeAble = pokeData.abilities
+            var pokeAble = pokeData.abilities;
 
             for(var i=0; i < pokeData.abilities.length; i++){
                 const current = pokeAble[i].ability.name;
@@ -152,10 +155,63 @@ async function getData() {
                     const abilityData = await response.json();
 
                     console.log(abilityData.effect_entries[2].effect);
+                    
+                    // carouselInd[0]: <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    // carouselInd[1-2]: <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+
+                    /* carouselArea[0]: <div class="carousel-item active">
+                                            <img src="./img/pose.png" class="d-block w-100" alt="..." style="opacity: 0.5;">
+                                            <div class="carousel-caption d-none d-md-block">
+                                                <h5>First slide label</h5>
+                                                <p>Some representative placeholder content for the first slide.</p>
+                                            </div>
+                                        </div>*/
+                    
+                    /* carouselArea[1-2]: <div class="carousel-item">
+                                            <img src="..." class="d-block w-100" alt="...">
+                                                <div class="carousel-caption d-none d-md-block">
+                                                    <h5>Second slide label</h5>
+                                                    <p>Some representative placeholder content for the second slide.</p>
+                                                </div>
+                                            </div>*/
+
+                    var carouselItem = document.createElement("div");
+                    carouselItem.classList.add("carousel-item");
+
+                    var carouselImage = document.createElement("img");
+                    carouselImage.setAttribute("src", "../img/pose.png");
+                    carouselImage.setAttribute("alt", "...");
+                    carouselImage.style.opacity="0.5";
+                    carouselImage.classList.add("d-block");
+                    carouselImage.classList.add("w-100");
+
+
+                    var carouselCap = document.createElement("div");
+                    carouselCap.classList.add("carousel-caption");
+                    carouselCap.classList.add("d-none");
+                    carouselCap.classList.add("d-md-block");
+
+
+                    var slideLabel = document.createElement("h5");
+                    slideLabel.innerHTML = "Test Label";
+
+                    var slideCon = document.createElement("p");
+                    slideCon.innerHTML="Test Content";
+
+
+                    if (i=0){
+                        carouselItem.classList.add("active");
+                    }
+                    carouselArea.appendChild(carouselItem);
+                    carouselItem.appendChild(carouselImage);
+                    carouselItem.appendChild(carouselCap);
+                    carouselCap.appendChild(slideLabel);
+                    carouselCap.appendChild(slideCon);
 
 
                 }catch (error) {
                     console.error(error.message);
+                    window.alert(error.message);
                 }
 
             }
